8f941bf9270c5876a6aa6fc7fd33fcb11986352e
cyw <cyw2902152274@gmail.com>
2026-08-02 03:32:00 +0800

    feat: make formal codecheck advisory

--- scripts/mae_flow_core/quality/__init__.py
+++ scripts/mae_flow_core/quality/__init__.py
@@ -1,1 +1,17 @@
 """Pure quality-contract primitives."""
+
+from .codecheck_advisory import (
+    CodeCheckDisposition,
+    CodeCheckTarget,
+    build_codecheck_target,
+    record_dispositions,
+    render_codecheck_request,
+)
+
+__all__ = [
+    "CodeCheckDisposition",
+    "CodeCheckTarget",
+    "build_codecheck_target",
+    "record_dispositions",
+    "render_codecheck_request",
+]
